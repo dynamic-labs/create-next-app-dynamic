b8abd133d94c713fa363136a6ba6368d755a99bb
Matthew Foyle <matthewfoyle@192.168.1.242>
2023-06-27 09:45:41 +0100

    style: hover variable fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Menu from '../components/Menu'
 
 export default function Home() {
 	return (
-		<main className="min-h-screen relative flex align-center justify-end flex-col">
+		<main className="min-h-screen relative flex align-center justify-center flex-col">
 			<Menu />
-			<Image src={'/home.png'} className="z-0 mt-12" fill alt="Home"></Image>
-			<div className="z-10 mb-32 mx-auto">
+			<Image src={'/home.png'} className="z-0" fill alt="Home"></Image>
+			<div className="z-10 mt-64 mx-auto">
 				<DynamicWidget />
 			</div>
 		</main>
