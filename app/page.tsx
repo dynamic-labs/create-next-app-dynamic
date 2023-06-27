e79c575736e498d9d6a8394e482146bd0bf705bb
Matthew Foyle <matthewfoyle@192.168.1.242>
2023-06-27 09:09:28 +0100

    chore: lint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
-import { DynamicWidget } from '../lib/dynamic';
-import Menu from '../components/Menu';
+import { DynamicWidget } from '../lib/dynamic'
+import Menu from '../components/Menu'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen relative flex align-center justify-end flex-col">
-      <Menu />
-      <Image src={'/home.png'} className='z-0 mt-12' fill alt='Home'></Image>
-        <div className='z-10 mb-32 mx-auto'>
-          <DynamicWidget /> 
-      </div>
-
-    </main>
-  )
+	return (
+		<main className="min-h-screen relative flex align-center justify-end flex-col">
+			<Menu />
+			<Image src={'/home.png'} className="z-0 mt-12" fill alt="Home"></Image>
+			<div className="z-10 mb-32 mx-auto">
+				<DynamicWidget />
+			</div>
+		</main>
+	)
 }
