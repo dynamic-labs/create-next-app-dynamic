1f068b06ea29088f1d265f7f41fef5f1ee1e57d2
Matthew Foyle <mfoyle93@gmail.com>
2023-11-14 13:52:38 +0100

    fix: small UI fix and package name

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 import { DynamicWidget } from '../lib/dynamic'
-import Menu from '../components/Menu'
 
 export default function Home() {
 	return (
 		<main className="min-h-screen relative flex align-center justify-end flex-col">
-			<Menu />
 			<Image src={'/home.png'} className="z-0" fill alt="Home"></Image>
 			<div className="z-10 mx-auto mb-20">
 				<DynamicWidget />
